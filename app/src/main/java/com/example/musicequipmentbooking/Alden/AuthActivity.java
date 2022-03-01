46dd7657f457b25a5ee707c872618679a34646ed
Alden <aldenc2023@student.cis.edu.hk>
2022-03-01 10:05:19 +0800

    Added AuthActivity and CISUser and Auth Layout

---
+++ app/src/main/java/com/example/musicequipmentbooking/Alden/AuthActivity.java
@@ -0,0 +1,114 @@
+package com.example.musicequipmentbooking.Alden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.musicequipmentbooking.MainActivity;
+import com.example.musicequipmentbooking.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firestore;
+
+    protected EditText emailField;
+    protected EditText passwordField;
+    protected String userType;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        emailField = findViewById(R.id.addEmailText);
+        passwordField = findViewById(R.id.addPasswordText);
+    }
+
+    /**
+     * This method allows users to sign in with their emails and password.
+     *
+     * @param v
+     */
+    //Allow user to sign in with a Google account
+    public void signIn(View v) {
+        System.out.println("Log in");
+        String emailString = emailField.getText().toString();
+        String passwordString = passwordField.getText().toString();
+
+        //Sign in with mAuth
+        mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("SIGN IN", "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                }
+                else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("SIGN IN", "signInWithEmail:failure", task.getException());
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    //Allow users to sign up with a Google account
+    public void signUp(View v) {
+        System.out.println("Sign Up");
+        String emailString = emailField.getText().toString();
+        String passwordString = passwordField.getText().toString();
+        String userTypeString = userType;
+
+        mAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d("SIGN UP", "Successfully signed up the user");
+                    updateUI(null);
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    String userIDString = user.getUid();
+
+                    CISUser newUser = new CISUser(emailString, passwordString, userIDString, userTypeString, 0, null);
+
+                    firestore.collection("Users").document(newUser.getUserID()).set(newUser);
+
+                } else {
+                    Log.w("SIGN UP", "createUserWithEmail:failure", task.getException());
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    //If user is signed in or created, goes to next screen
+    public void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
--- app/src/main/java/com/example/musicequipmentbooking/Alden/CISUser.java
+++ app/src/main/java/com/example/musicequipmentbooking/Alden/CISUser.java
@@ -1,7 +1,85 @@
 package com.example.musicequipmentbooking.Alden;
 
+import java.util.ArrayList;
+
 public class CISUser {
+    private String email;
+    private String password;
+    private String userID;
+    private String userType;
+    private int creditScore;
+    ArrayList instrumentsBorrowed = new ArrayList();
 
+    //No argument for firestore
+    public CISUser(){
+    }
 
+    public CISUser(String email, String password, String userID, String userType, int creditScore, ArrayList instrumentsBorrowed) {
+        this.email = email;
+        this.password = password;
+        this.userID = userID;
+        this.userType = userType;
+        this.creditScore = creditScore;
+        this.instrumentsBorrowed = instrumentsBorrowed;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public ArrayList getInstrumentsBorrowed() {
+        return instrumentsBorrowed;
+    }
+
+    public void setInstrumentsBorrowed(ArrayList instrumentsBorrowed) {
+        this.instrumentsBorrowed = instrumentsBorrowed;
+    }
+
+    @Override
+    public String toString() {
+        return "CISUser{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", userID='" + userID + '\'' +
+                ", userType='" + userType + '\'' +
+                ", creditScore=" + creditScore +
+                ", instrumentsBorrowed=" + instrumentsBorrowed +
+                '}';
+    }
 }
