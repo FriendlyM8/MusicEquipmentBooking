3812b5978f52df0d1bbdeb95465cbefef191b058
Alden <aldenc2023@student.cis.edu.hk>
2022-03-03 12:11:14 +0800

    Added InstrumentListActivity and deleted Student and Teahcer and SignUp. Additionally, also added AddInstrumentsActivity.

---
+++ app/src/main/java/com/example/musicequipmentbooking/Ryan/AddInstrumentsActivity.java
@@ -0,0 +1,16 @@
+package com.example.musicequipmentbooking.Ryan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.musicequipmentbooking.R;
+
+public class AddInstrumentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_instruments);
+    }
+}
