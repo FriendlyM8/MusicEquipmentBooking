61ebedc97ba5aaa6385db4dc3d04151ccb7a508b
ryancheuk <rstcheuk13@gmail.com>
2022-03-19 21:56:51 +0800

    User profile

--- app/src/main/java/com/example/musicequipmentbooking/Ryan/UserProfileActivity.java
+++ app/src/main/java/com/example/musicequipmentbooking/Ryan/UserProfileActivity.java
@@ -23,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
 
 
 public class UserProfileActivity extends AppCompatActivity {
